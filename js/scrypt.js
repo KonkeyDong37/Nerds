a5013c16f2c2430564bfe1b873d36e0b7d139c23
Andrey <KonkeyDong37@gmail.com>
2018-06-05 03:27:08 +0500

    popup add animation

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,17 +1,28 @@
-var popap = document.querySelector(".modal")
+var popup = document.querySelector(".modal")
 var modal_show = document.querySelector(".button-contacts")
 var modal_close = document.querySelector(".modal-close")
+var modal_name = document.querySelector(".modal-name")
 var catalog_item = document.querySelectorAll(".catalog-item")
 var item_info = document.querySelectorAll(".catalog-item-info")
 
 modal_show.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popap.classList.add("js-modal-show");
+  popup.classList.add("js-modal-show");
+  modal_name.focus();
 });
 
 modal_close.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popap.classList.remove("js-modal-show");
+  popup.classList.remove("js-modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+          evt.preventDefault();
+    if (popup.classList.contains("js-modal-show")) {
+      popup.classList.remove("js-modal-show");
+    }
+  }
 });
 
 for (var i = 0; i < catalog_item.length; i++) {
