4e61e5e1affb4be13b91d2063c7065852237fa9e
Andrey <KonkeyDong37@gmail.com>
2018-06-05 02:35:37 +0500

    add js

---
+++ js/scrypt.js
@@ -0,0 +1,29 @@
+var popap = document.querySelector(".modal")
+var modal_show = document.querySelector(".button-contacts")
+var catalog_item = document.querySelectorAll(".catalog-item")
+var item_info = document.querySelectorAll(".catalog-item-info")
+
+modal_show.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  console.log("It's work");
+  popap.classList.add("js-modal-show");
+});
+
+for (var i = 0; i < catalog_item.length; i++) {
+    catalog_item[i].addEventListener('mouseover', function(evt) {
+          event.preventDefault();
+          item_info.classList.add("js-catalog-item-info-slide-up");
+      });
+  }
+
+// catalog_item.addEventListener("mouseover", function (evt) {
+//   evt.preventDefault();
+//   console.log("Yep");
+//   item_info.classList.add("js-catalog-item-info-slide-up");
+// });
+//
+// catalog_item.addEventListener("mouseout", function (evt) {
+//   evt.preventDefault();
+//   item_info.classList.remove("js-catalog-item-info-slide-up");
+//   item_info.classList.add("js-catalog-item-info-slide-down")
+// });
